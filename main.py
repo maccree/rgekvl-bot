458d3d9385d2cedcad36586cf5b65beca3a8201c
maccree <maccree.a@yandex.ru>
2020-04-06 22:08:59 +0300

    Addition to the console log

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
     async def on_raw_reaction_add(self, payload):
+        if payload.message_id == config.POST_ID:
+            channel = self.get_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            member = utils.get(message.guild.members, id=payload.user_id)
